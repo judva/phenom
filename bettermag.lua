90661f9a5e251398a9266231afc7aa0f572cd639
67ve <166963561+67ve@users.noreply.github.com>
2024-04-21 17:59:22 -0700

    Update bettermag.lua

--- bettermag.lua
+++ bettermag.lua
@@ -2,7 +2,7 @@ game:GetService("UserInputService").InputBegan:connect(onKeyPress)
 
 function onKeyPress(inputObject, gameProcessedEvent)
 if gameProcessedEvent then return end
-if inputObject.KeyCode == Enum.KeyCode.M then
+if inputObject.KeyCode == Enum.KeyCode.R then
 local Torso = game.Players.LocalPlayer.Character.Torso
 for i, v in pairs(workspace:GetDescendants()) do
     if v.Name == "rim" then
